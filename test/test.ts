80b46de880ca635c44355bf62f9e5774a5d571a3
nanlei <36439285+nanlei2000@users.noreply.github.com>
2019-09-06 13:02:20 +0800

    feat: 实现`all` `race` `resolve` `reject`

--- test/test.ts
+++ test/test.ts
@@ -1,19 +1,27 @@
 import MyPromise = require('../src/index');
-new MyPromise<string>(resolve => {
-  setTimeout(() => {
-    resolve('hello');
-  });
-})
-  .then(str => {
-    return str.length;
-  })
-  .then(num => {
-    console.log('🤓🤔😓: num', num);
-    throw new Error('oops!');
-  })
-  .catch(error => {
-    console.log(error);
-  });
+// new MyPromise<string>(resolve => {
+//   setTimeout(() => {
+//     resolve('hello');
+//   });
+// })
+//   .then(str => {
+//     return str.length;
+//   })
+//   .then(num => {
+//     console.log('🤓🤔😓: num', num);
+//     throw new Error('oops!');
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
 
 Promise.prototype.then;
 Promise.resolve;
+Promise.all;
+MyPromise.reject(`hehe`).catch(reason => {
+  console.log('🤓🤔😓: reason', reason);
+});
+MyPromise.resolve(MyPromise.resolve(111)).then(value => {
+  console.log(value);
+});
+Promise.reject;
